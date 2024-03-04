86bb2bbb5780bbe2e68ee06a9d1d046fed553122
Vegas-in <angeldevegas@gmail.com>
2024-03-04 23:33:56 +0100

    ejercicio resulto

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
-const cardTemplate = function (/* You can pass the data here*/) {
+const cardTemplate = function (data) { //teniamos una funcion creada que tiene como parámtro los datos de un objeto y devuelve el texto en una tarjeta creada en html
   return `<div class="card">
-              <img id="flag-image" src="${imgSrc}" alt="flag" />
-              <h1 class="center">${countrieName}</h1>
-            </div>`;
+              <img id="${data.flags.png}" src="${data.flags.png}" alt="flag" />
+              <h1 class="center">${data.name.official}</h1>
+            </div>`; //pinto las cards accediendo a los datos del array de objetos y a sus propiedades
 };
-
-
+//accedemos a las countries y le asignamos una variable
 const countriesNode = document.getElementById("countries");
 
-
-
-
-fetch("https://restcountries.com/v3.1/all")
+fetch('https://restcountries.com/v3.1/all')//hago un fetch con la url para hacer la solicitud a la API
   .then(function (response) {
-    return response.json()
+    return response.json();//después convierte la respuesta en formato json
     // fetch() returns a promise containing the response (a Response object).
     // This is just an HTTP response, not the actual JSON. 
     // To extract the JSON body content from the response, 
     // we use the json() method and pass it into the next .then()
-
   })
   .then(function (countries) {
-    for (let i = 0; i < countries.length; i++) {
-      imgSrc = countries.flags.png;
-      countrieName = countries.name.common;
-      let divCountrie = cardTemplate(imgSrc,countrieName)
-      countriesNode.appendChild(divCountrie)
-      document.getElementById("countries").innerHTML
-    }
-
+    let cards = ""; //en esta variable almaceno las tarjetas de los paises
+    countries.forEach(country => { //itero sobre cada pais para luego ir pintando las tarjetas de cada uno
+      // 1 - We will need to iterate the countries variable with a loop
+      console.log(countries);
+      cards += cardTemplate(country);//uso la función de cardTemplate
+      // 2 - You can use the cardTemplate() function to create a div with a class card already styled
+      // 💡 you can use countriesNode variable to add elements
+    });
+    countriesNode.innerHTML +=cards;///Añadir al DOM EL RESULTADO, le agregamos a las tarjetas la función creada al principio con la info que pintaremos en el DOM
     // Here is where you'll need to add into the DOM all the countries received from API 
-
-    // 1 - We will need to iterate the countries variable with a loop
-    // 2 - You can use the cardTemplate() function to create a div with a class card already styled
-    // 💡 you can use countriesNode variable to add elements
   });
